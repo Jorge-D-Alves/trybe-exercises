32f1ce3af37005d62f9a4989df1c3aca982fb77c
Jorge Alves <alvesdjorge@gmail.com>
2020-08-28 10:51:29 -0300

    Exercicios 1 e 2

---
+++ bloco_4/dia_1/exe1.js
@@ -0,0 +1,18 @@
+let a = 10;
+let b = 10;
+
+let resultado
+resultado = a+b;
+console.log(resultado);
+
+resultado = a-b;
+console.log(resultado);
+
+resultado = a*b;
+console.log(resultado);
+
+resultado = a/b;
+console.log(resultado);
+
+resultado = a%b;
+console.log(resultado);
